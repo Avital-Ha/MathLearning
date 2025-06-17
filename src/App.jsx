bd99e0cbe9792004de03042fcb3f3fa153125147
Avital-Ha <avital.hamburg@gmail.com>
2025-06-17 10:15:50 +0300

    עדכון שינויים

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Pages/Home";
 
@@ -21,7 +21,7 @@ function Setup() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         {/* דף Setup בלי Layout */}
         <Route path="/setup" element={<Setup />} />
@@ -40,6 +40,6 @@ export default function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
