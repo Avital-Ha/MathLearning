097ade3f6e822ecb00abd0bda5bf606004812c69
Avital-Ha <avital.hamburg@gmail.com>
2025-06-17 10:19:47 +0300

    updated Hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Pages/Home";
 
